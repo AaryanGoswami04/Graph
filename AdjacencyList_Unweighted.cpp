3c13a1fac4939877bad17000aef4220d3cd5368d
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2022-12-08 10:18:37 +0530

    Update AdjacencyList_Unweighted.cpp

--- AdjacencyList_Unweighted.cpp
+++ AdjacencyList_Unweighted.cpp
@@ -4,6 +4,14 @@ using namespace std;
 //Below code is for unweighted graph
 //Directed graph: SC:O(E)
 //Undirected graph: SC:O(2E)
+void PrintAdjList(int n){
+  for (int i = 0; i < n; i++) {
+    cout << i << "->";
+    for (auto j : adj[i])
+      cout << j << ",";
+    cout << endl;
+  }
+}
 int main() {
   //n: no. of vertices
   //m: number of edges
@@ -14,7 +22,7 @@ int main() {
   int direction;
   cin>>direction;
   
-  vector<int>adj[n+1]; //1 based indexing
+  vector<int>adj[n]; //0 based indexing
   for(int i=0; i<m; i++){
     int u,v;
     cin>>u>>v;
@@ -22,4 +30,5 @@ int main() {
     if(direction==0)    //undirected graph
       adj[v].push_back(u);
   }
+  PrintAdjList(n);
 }
