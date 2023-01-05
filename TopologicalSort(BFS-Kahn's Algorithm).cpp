58cb47f3841ab8a0a6bc3109aa596281572fbac6
Aaryan Goswami <114975960+AaryanGoswami04@users.noreply.github.com>
2023-01-05 11:30:00 +0530

    Create TopologicalSort(BFS-Kahn's Algorithm).cpp

---
+++ TopologicalSort(BFS-Kahn's Algorithm).cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>indegree(V,0);
+	    for(int i=0; i<V; i++){
+	        for(auto it:adj[i])
+	           indegree[it]++;
+	    }
+	    
+	    queue<int>q;
+	    for(int i=0; i<V; i++){
+	        if(!indegree[i])
+	            q.push(i);
+	    }
+	        
+	    vector<int>topo;
+	    while(!q.empty()){
+	        int front = q.front();
+	        q.pop();
+	        topo.push_back(front);
+	        
+	        for(auto i:adj[front]){
+	            indegree[i]--;
+	            if(!indegree[i])
+	                q.push(i);
+	        }
+	        
+	    }
+	    return topo;
+	}
+};
